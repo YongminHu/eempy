d90a18167f4c68d2378bdb2ef2ee6cb1a6a998cb
Yongmin Hu <yongminhu@outlook.com>
2024-03-27 15:31:49 +0100

    2024.03.27 Hotfix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='eem-python',
-    version='1.21',
+    version='1.22',
     author='Yongmin Hu',
     author_email='yongminhu@outlook.com',
     packages=find_packages(),
